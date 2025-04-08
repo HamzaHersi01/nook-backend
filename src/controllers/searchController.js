3ffafa04d6663b473189859457381c43c48e0f30
Hamza <hamza.hersi@mail.bcu.ac.uk>
2025-04-08 09:42:10 +0100

    Added comments in the searchControllers file, created the works route file

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -5,17 +5,20 @@ const {getAuthorSearch} = require('../utils/api/openLibraryService');
 exports.searchBookByISBN = async (req, res) => {
   const { isbn } = req.params;
   const bookInfo = await getBookByISBN(isbn);
+  //TODO: Implement the API call that gets the work from this response
   res.json(bookInfo);
 };
 
 exports.searchBooksByTitle = async (req, res) => {
   const { item } = req.params; 
   const searchResponse = await getBookBySearch(item);
+  //TODO: create array of search entries and send that as a response
   res.json(searchResponse);
 };
 
 exports.searchAuthor = async (req, res) =>{
   const { author } = req.params;
   const authorSearchResponse = await getAuthorSearch(author);
+  //TODO: transform data if need be. unsure if i need this one to be honest but will keep for time being
   res.json(authorSearchResponse)
 }
